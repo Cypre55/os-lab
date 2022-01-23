0bf8a51a83952848327c56b39c84c97d3a44afb8
Chappidi Yoga Satwik <satwik.chappidi@iitkgp.ac.in>
2022-01-23 23:48:35 +0530

    Changed 1c and 1e

--- 1st Lab/Assgn1_1ewv_52_19CS30013_19CS10045.sh
+++ 1st Lab/Assgn1_1ewv_52_19CS30013_19CS10045.sh
@@ -4,4 +4,6 @@ curl "http://headers.jsontest.com">tmp.json
 IFS=,
 for i in $REQ_HEADERS; do result=$(cat tmp.json|grep $i|awk '{print $2}');echo $i:$result;done;
 IFS=$' \t\n'
+echo>valid.txt
+echo>invalid.txt
 for i in $(ls JSONData); do cat ./JSONData/$i|jq -e .>/dev/null 2>&1;if [[ "${PIPESTATUS[1]}" -eq 0 ]];then echo $i>>valid.txt;else echo $i>>invalid.txt;fi;done;
