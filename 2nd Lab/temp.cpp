fd04a009ba97fa3b908bb8e77e02102ffecab176
Nikhil Tudaha <nikhil.janya@gmail.com>
2022-02-14 02:29:54 +0530

    Started MultiWatch

--- 2nd Lab/temp.cpp
+++ 2nd Lab/temp.cpp
@@ -8,6 +8,14 @@
 #include <fcntl.h> // for open, close etc
 #include <unistd.h> // for exec family of commands
 
+// TODO
+// make an tokenise command function which takes care of ' and "
+// remove bits/stdc++.h
+
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+
 using namespace std;
 
 void getCommand (string str, vector<string> &cmd) {
@@ -154,7 +162,7 @@ void executeCommand(char *cmd_arr[], int cmd_len, int infd = 0, int outfd = 1)
         if (outfile != nullptr)
         {
             cout << "Writing output to " << outfile << endl;
-            outfd = open(outfile, O_WRONLY | O_CREAT);
+            outfd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC);
             if (outfd == -1)
             {
                 cout << "cannot open the output file" << endl;
@@ -163,9 +171,20 @@ void executeCommand(char *cmd_arr[], int cmd_len, int infd = 0, int outfd = 1)
             dup2(outfd, STDOUT_FILENO);
         }
         
-        // cerr << "PID of child = " << getpid() << endl;
-        execvp(cmd_arr[0], cmd_arr);
-        exit(0);
+        cout << "PID of child = " << getpid() << endl;
+        if (fork() == 0)
+        {
+            execvp(cmd_arr[0], cmd_arr);
+        }
+        else
+        {
+            wait(NULL);
+            // dup2(infd, 0);
+            // close(infd);
+            // dup2(outfd, 1);
+            // close(outfd);
+            exit(0);
+        }
     }
     else if (x > 0)
     {
@@ -247,6 +266,121 @@ int main (void) {
 
         }
 
+
+    }
+
+}
+
+void executeMultiWatch(string command)
+{
+    // syntax is multiWatch ["cmd1", "cmd2", "cmd3"]
+    vector <string> commands;
+    int n = command.length();
+    for (int i = 0; i < n; )
+    {
+        if (command[i] == '\"')
+        {
+            // TODO check this
+            // find the next ','
+            // better than looking for '\"' as these can be nested
+            int j;
+            bool found = false;
+            for (j = i + 1; j < n; j++)
+            {
+                if (command[j] == ',')
+                {
+                    commands.push_back(command.substr(i + 1, j - i - 2));
+                    found = true;
+                    i = j;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                commands.push_back(command.substr(i + 1, n - i - 3));
+                i = n;
+            }
+        }
+    }
+
+    // DEBUG
+    cout << "multiwatch commands are:" << endl;
+    for (auto x : commands)
+    {
+        cout << x << endl;
+    }
+    cout << endl;
+
+    int command_count = commands.size();
+
+    int inotify_fd = inotify_init();
+    if (inotify_fd == -1)
+    {
+        perror("inotify_init");
+    }
+
+    vector <int> readFD (command_count), writeFD (command_count), inotify_WD (command_count);
+    for (int i = 0; i < command_count; i++)
+    {
+        string filename = ".temp.PID" + to_string(i + 1) + ".txt";
+        writeFD[i] = open(filename.c_str(), O_WRONLY | O_CREAT, 0666);
+        readFD[i] = open(filename.c_str(), O_RDONLY, 0666);
+        int inotify_wd = inotify_add_watch(inotify_fd, filename.c_str(), IN_MODIFY);
+        inotify_WD[i] = inotify_wd;
+    }
+
+    vector <pid_t> process_ids (command_count);
+    for (int i = 0; i < command_count; i++)
+    {
+        process_ids[i] = fork();
+        if (process_ids[i] < 0)
+        {
+            printf("Error in generating the process.\n");
+            exit(0);
+        }
+        if (process_ids[i] == 0)
+        {
+            // TODO execute commands[i].
+            dup2(writeFD[i], STDOUT_FILENO);
+        }
+    }
+
+    int length, i = 0;
+    char buffer[BUF_LEN];
+
+    while (true)
+    {
+        __attribute__((aligned((struct inotify_event))));
+        length = read(inotify_fd, buffer, BUF_LEN);
+
+        if (length < 0)
+        {
+            perror("read");
+        }
+
+        while (i < length)
+        {
+            struct inotify_event *event = (struct inotify_event *) &buffer[i];
+
+            if (event->len)
+            {
+                if (event->mask & IN_MODIFY)
+                {
+                    int file_WD = event->wd;
+                    int file_index = -1;
+                    for (int x = 0; x < command_count; x++)
+                    {
+                        if (inotify_WD[x] == file_WD)
+                        {
+                            file_index = x;
+                        }
+                    }
+                }
+
+                // print the output of file_index in this format
+            }
+            i += sizeof(struct inotify_event) + event->len;
+        }
 
     }
 
